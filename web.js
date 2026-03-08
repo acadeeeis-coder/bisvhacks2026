8f6c61f1911f1c246e77c0de4ca94e002a8d3e6f
Dracfinity <138898973+Dracfinity@users.noreply.github.com>
2026-03-08 09:35:39 -0700

    Refactor APIcall to include question parameter
    Modify APIcall to accept a question parameter and update contents format. Call APIcall with 'Restaurants' and log the response.

--- web.js
+++ web.js
@@ -7,12 +7,13 @@ function Find() {
   let position = navigator.geolocation.getCurrentPosition()
   return position;
 }
-async function APIcall() {
+async function APIcall(question) {
   const response = await ai.models.generateContent({
     model: "gemini-3-flash-preview",
-    contents: input+'at this location: '+ Find(),
+    contents: `${question} near the latitude and longitude ${Find()}`,
   });
   return response.text;
 }
 
-await APIcall();
+let response = await APIcall("Restraunts");
+console.log(response)
